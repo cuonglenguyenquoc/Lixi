ee2161063a54f39cb9cc4481fbdf348c818f3acc
Leo <cuong.lenguyenquoc@gmail.com>
2023-12-07 11:01:46 +0700

    initial code

---
+++ Lixi/LixiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LixiApp.swift
+//  Lixi
+//
+//  Created by Macbook on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LixiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-Lixi/Pods-Lixi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lixi : NSObject
+@end
+@implementation PodsDummy_Pods_Lixi
+@end
